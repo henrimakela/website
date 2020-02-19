beb5ca0934cbdd09c9e250982126d1be9eb13320
Henri Mäkelä <henri.makela@boogiesoftware.com>
2020-02-19 22:06:32 +0200

    contact page layout

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -7,15 +7,12 @@ class Contact extends Component {
             <div className="contact_container">
             <div className="content_section">
                 <div className="text_div">
-                <h1>GET IN <span style={{color: "#17B890"}}>TOUCH</span></h1>
+                <h1>Contact info.</h1>
+                <p>For <span style={{fontWeight: 800}}>professional</span> business, you can contact me from the links below. </p>
+                <p>For <span style={{fontWeight: 800}}>unprofessional</span> business, you can contact me from the links below. </p>
                 </div>
-
-                <div className="icon_row">
-                   <a className="contact_link" href="https://linkedin.com"><img className="icon" src="/images/linked_in.png" alt="Contact me"/>  <h4>LinkedIn</h4></a> 
-                   <a className="contact_link" href="https://github.com/henrimakela"><img className="icon" src="/images/github.png" alt="Contact me"/> <h4>Github</h4></a> 
-                   <a className="contact_link" href="https://instagram.com/henmakela"><img className="icon" src="/images/instagram.png" alt="Contact me"/><h4>Instagram</h4></a> 
-                   <a className="contact_link" href="https://google.com"><img className="icon" src="/images/mail.png" alt="Contact me"/><h4>Email</h4></a> 
-                </div>
+                    <IconRow/>
+                
                 </div>
             
             </div>
@@ -24,3 +21,19 @@ class Contact extends Component {
 }
 
 export default Contact;
+
+
+
+class IconRow extends Component {
+    render() {
+        return (
+            <div className="icon_row">
+                   <a className="contact_link" href="https://www.linkedin.com/in/henri-m%C3%A4kel%C3%A4-92aa42114/"><img className="icon" src="/images/linked_in.png" alt="Contact me"/> <h4>LinkedIn</h4></a> 
+                   <a className="contact_link" href="https://github.com/henrimakela"><img className="icon" src="/images/github.png" alt="Contact me"/> <h4>Github</h4></a> 
+                   <a className="contact_link" href="https://instagram.com/henmakela"><img className="icon" src="/images/instagram.png" alt="Contact me"/><h4>Instagram</h4></a> 
+                   <a className="contact_link" href="mailto:henri.o.makela@gmail.com"><img className="icon" src="/images/mail.png" alt="Contact me"/><h4>Email</h4></a> 
+                </div>
+        );
+    }
+}
+
