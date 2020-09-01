b600dee24afeebaccfd26bf1d9e3782836d2506b
Henri Mäkelä <henri.makela@boogiesoftware.com>
2020-09-01 21:04:32 +0300

    refactor portfolio page and components

--- src/components/portfolio/portfolio_card.js
+++ src/components/portfolio/portfolio_card.js
@@ -1,23 +1,101 @@
 import React, { Component } from 'react';
-import './portfolio_card.css'
+import styled from 'styled-components';
+
+const Card = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin: 20px auto;
+    border-radius: 32px;
+    justify-content: center;
+    width: 80%;
+    padding: 64px 0px;
+    -webkit-box-shadow: 5px 5px 30px 0px rgba(163,163,163,1);
+    -moz-box-shadow: 5px 5px 30px 0px rgba(163,163,163,1);
+    box-shadow: 5px 5px 30px 0px rgba(163,163,163,1);
+
+    @media(max-width: 900px){
+        padding: 24px 0px;
+        margin: 0px;
+        width: 100%;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        box-shadow: none;
+        background: white !important;
+        border-radius: 0px;
+        border-bottom: 1px solid #E0E0E0;
+    }
+`;
+
+
+const CardImage = styled.img`
+    margin-left: 15%;
+    max-width: 35%;
+
+
+    @media(max-width: 900px){
+        margin: 0px;
+        max-width: 40%;
+    }
+
+    @media(max-width: 480px){
+        max-width: 80%;
+    }
+`;
+
+const TextArea = styled.div`
+    max-width: 400px;
+    margin: 0px 32px;
+
+    @media(max-width: 900px){
+        max-width: 80%;
+        text-align: center;
+
+        & > a{
+            display: flex;
+            justify-content: center;
+        }
+    }
+`;
+
+const Subtitle = styled.h4`
+
+    @media(max-width:900px){
+        display: none;
+    }
+`;
+
+const Paragraph = styled.p`
+
+    @media(max-width:900px){
+        display: none;
+    }
+`;
+
+const Badge = styled.img`
+    height:80px;
+    width: 200px;
+`;
+
+
 class PortfolioCard extends Component {
     render() {
         return (
-            <div className="portfolio_card" style={{background: `linear-gradient(90deg, ${this.props.background} 35%, #fff 0%)`}}>
-                <img className="portfolio_card_image" src={"/images/" + this.props.image}/>
-                <div className="portfolio_card_text_container">
+            <Card style={{background: `linear-gradient(90deg, ${this.props.background} 35%, #fff 0%)`}}>
+                <CardImage src={"/images/" + this.props.image}/>
+                <TextArea>
                     <h4>{this.props.title}</h4>
-                    <h4 className="hide_on_mobile" style={{color: this.props.subtitleColor}}>Currently</h4>
-                    <p className="hide_on_mobile">{this.props.currently}</p>
-                    <h4 className="hide_on_mobile" style={{color: this.props.subtitleColor}}>In the future</h4>
-                    <p className="hide_on_mobile">{this.props.future}</p>
-                    <h4 className="hide_on_mobile" style={{color: this.props.subtitleColor}}>Tech</h4>
-                    <p className="hide_on_mobile">{this.props.tech}</p>
-                    <h4 className="hide_on_mobile" style={{color: this.props.subtitleColor}}>Keywords</h4>
+                    <Subtitle style={{color: this.props.subtitleColor}}>Currently</Subtitle>
+                    <Paragraph>{this.props.currently}</Paragraph>
+                    <Subtitle style={{color: this.props.subtitleColor}}>In the future</Subtitle>
+                    <Paragraph>{this.props.future}</Paragraph>
+                    <Subtitle style={{color: this.props.subtitleColor}}>Tech</Subtitle>
+                    <Paragraph>{this.props.tech}</Paragraph>
+                    <Subtitle style={{color: this.props.subtitleColor, fontWeight: 800}}>Keywords</Subtitle>
                     <p style={{color: this.props.subtitleColor, fontWeight: "bold"}}>{this.props.keywords}</p>
-                    <a href={this.props.playUrl}><img className="google_play_badge" src="/images/google-play-badge.png"/></a>
-                </div>
-            </div>
+                    <a href={this.props.playUrl}><Badge src="/images/google-play-badge.png"/></a>
+                </TextArea>
+            </Card>
         );
     }
 }
