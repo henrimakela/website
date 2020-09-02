28ccc398ce0bfa6a1499f4fea46389d3985e1f3e
Henri Mäkelä <henri.makela@boogiesoftware.com>
2020-09-02 20:31:52 +0300

    refactor skill page to use styled components

--- src/components/services/service_section.js
+++ src/components/services/service_section.js
@@ -1,19 +1,65 @@
 
 import React, { Component } from 'react';
-import './service_section.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    margin: 16px;
+    padding:16px;
+    padding-bottom: 32px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    border-bottom: 1px solid #E0E0E0;
+
+    @media(max-width:767px){
+        justify-content: center;
+        flex-direction: column-reverse;
+}
+`;
+
+const TextArea = styled.div`
+    padding: 16px;
+    max-width: 200px;
+    display: flex;
+    flex-direction: column;
+
+    & > h3{
+        font-size: 28px;
+        font-weight: bolder;
+        margin:0px;
+    }
+
+    & > p{
+        font-size: 18px;
+        font-weight: 600;
+    }
+
+    @media(max-width:767px){
+    & > h3, p{
+        text-align: center;
+    }
+    
+}
+`;
+
+const Image = styled.img`
+    height: auto;
+    max-height: 250px;
+    max-width: 350px;
+`;
 
 class ServiceSection extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="text_container">
+            <Container>
+                <TextArea>
                 <h3>{this.props.title}</h3>
                 {this.props.description}
-                </div>
+                </TextArea>
              
-                <img className="section_image" src={"/images/" + this.props.image} alt="Section illustration"/>
+                <Image src={"/images/" + this.props.image} alt="Section illustration"/>
                
-            </div>
+            </Container>
         );
     }
 }
