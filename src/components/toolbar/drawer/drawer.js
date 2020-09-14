bcd119aa2bcb1f460f7fdbe43f3e3d9db3bfa2c3
Henri Mäkelä <henri.o.makela@gmail.com>
2020-09-14 20:58:37 +0300

    remove unused pages from the toolbar

--- src/components/toolbar/drawer/drawer.js
+++ src/components/toolbar/drawer/drawer.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import './drawer.css';
+import React, { Component } from "react";
+import "./drawer.css";
 
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class Drawer extends Component {
-
-    render() {
-        let drawerClasses = "side_drawer";
-        if(this.props.show === true){
-            drawerClasses = "side_drawer open";
-        }
-
-        return (
-           <nav className={drawerClasses}>
-               <ul>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/blog">Blog</Link></li>
-                    <li><Link to="/services">Skills</Link></li>
-                    <li><Link to="/portfolio">Portfolio</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>   
-                </ul>  
-           </nav>
-        );
+  render() {
+    let drawerClasses = "side_drawer";
+    if (this.props.show === true) {
+      drawerClasses = "side_drawer open";
     }
+
+    return (
+      <nav className={drawerClasses}>
+        <ul>
+          <li>
+            <Link to="/about">ABOUT</Link>
+          </li>
+          <li>
+            <Link to="/expertise">EXPERTISE</Link>
+          </li>
+          <li>
+            <Link to="/portfolio">PORTFOLIO</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
 export default Drawer;
--- src/components/toolbar/toolbar.js
+++ src/components/toolbar/toolbar.js
@@ -1,36 +1,38 @@
-
-import React, { Component } from 'react';
-import DrawerToggle from './drawer/drawer_toggle';
-import {Link} from 'react-router-dom';
-import './toolbar.css'
-
+import React, { Component } from "react";
+import DrawerToggle from "./drawer/drawer_toggle";
+import { Link } from "react-router-dom";
+import "./toolbar.css";
 
 class Toolbar extends Component {
-
-    render() {
-        return (
-            <header className="toolbar">
-                <nav className="toolbar_navigation">
-                    <div className="burger_bar">
-                        <DrawerToggle click={this.props.drawerClickHandler}/>
-                    </div>
-                    <div className="spacer_mobile"></div>
-                    <div className="toolbar_logo"><a href="/">DARK</a></div>
-                    <div className="spacer"></div>
-                    <div className="toolbar_nav_items">
-                        <ul>
-                            <li><Link to="/about">ABOUT</Link></li>
-                            <li><Link to="/blog">BLOG</Link></li>
-                            <li><Link to="/expertise">EXPERTISE</Link></li>
-                            <li><Link to="/portfolio">PORTFOLIO</Link></li>
-                            <li><Link to="/contact">CONTACT</Link></li>    
-                        </ul>                 
-                    </div>
-                   
-                </nav>
-            </header>
-        );
-    }
+  render() {
+    return (
+      <header className="toolbar">
+        <nav className="toolbar_navigation">
+          <div className="burger_bar">
+            <DrawerToggle click={this.props.drawerClickHandler} />
+          </div>
+          <div className="spacer_mobile"></div>
+          <div className="toolbar_logo">
+            <a href="/">DARK</a>
+          </div>
+          <div className="spacer"></div>
+          <div className="toolbar_nav_items">
+            <ul>
+              <li>
+                <Link to="/about">ABOUT</Link>
+              </li>
+              <li>
+                <Link to="/expertise">EXPERTISE</Link>
+              </li>
+              <li>
+                <Link to="/portfolio">PORTFOLIO</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    );
+  }
 }
 
 export default Toolbar;
